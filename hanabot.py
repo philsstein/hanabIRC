025cafc88e546a2c4ea9c2d637bdaeb91f4d8863
Geoff Lawler <glawler@tislabs.com>
2013-01-24 16:13:47 -0500

    abstracted colorizing and bolding. The hanabi game modules are now abstracted from the IRC interface. They spaek straight text arrays which the IRC server then prints to the appropriate nick/channel.

--- hanabot.py
+++ hanabot.py
@@ -4,8 +4,8 @@ import irc
 import time
 import string
 import random
-
 from hanabi_game import Game
+from text_markup import irc_markup
 from irc.bot import SingleServerIRCBot
 
 log = logging.getLogger(__name__)
@@ -71,7 +71,7 @@ class Hanabot(SingleServerIRCBot):
 		# I don't understand when args will ever be more than just a string of space separated words
 		cmds = cmds[0].split()
 
-		# op only commands
+		# op only commands - return after executing.
 		if cmds[0] in self.admin_commands:
 			for chname, chobj in self.channels.items():
 				if nick in chobj.opers():
@@ -79,15 +79,16 @@ class Hanabot(SingleServerIRCBot):
 						self.die('Seppuku Complete')
 					elif cmds[0] == 'show': 
 						for g in self.games.values():
-							g.show(nick)
+							g.show_game_state()
 
-						return	
+					return	
 	
 		# user commands
 		if not cmds[0] in self.commands:
-			self.connection.notice(nick, 'My dearest brother \x035Willis\x03, I do not understand this "\x036,8%s\x03" of which you speak.' % ' '.join(cmds))
+			self.connection.notice(nick, 'My dearest brother Willis, I do not understand this "%s" of which you speak.' % ' '.join(cmds))
 			return
 
+		# call the appropriate handle_* function. 
 		method = getattr(self, 'handle_%s' % cmds[0], None)
 		if method:
 			method(cmds[1:], event)
@@ -96,23 +97,38 @@ class Hanabot(SingleServerIRCBot):
 	#############################################################
 
 	def handle_status(self, args, event):
+		'''
+		Show status of all/one of games user is playing in.
+			args: [name (optional)]
+		If game name is given, show just that game status. 		
+		'''	
 		log.debug('got status event')
+		game_name = None if len(args) == 0 else args[0]
+		nick = event.source.nick
 		for g in self.games:
-			self.connection.notice(nick, '%s' % g)
+			if nick in g.players:
+				if (game_name and game_name == g.name) or not game_name:
+					for l in g.get_status(nick):
+						self.connection.notice(nick, l)
 
 	def handle_new(self, args, event):
-		'''args: [game name]'''
+		''' 
+		Create a new game. 
+			args: [game name]
+		If given, use the name as to id the game instance. 
+		'''
 		log.debug('got new game event')
 		game = None if len(args) == 0 else args[0]
+		nick = event.source.nick
 		if not game: 
 			game = ''.join(random.choice(string.lowercase) for i in range(8))
 
 		if game in self.games.keys():
-			self.connection.notice(event.source.nick, 'The game %s already exists.' % game)
+			self.connection.notice(nick, 'The game %s already exists.' % game)
 		else:
 			log.info('Starting new game %s' % game)
-			self.games[game] = Game(self.connection, game, self.channel)
-			self.connection.notice(self.channel, 'New game "%s" started. Accepting joins now.' % game)
+			self.games[game] = Game(game, irc_markup())
+			self.connection.notice(self.channel, 'New game "%s" started by %s. Accepting joins now.' % (nick, game))
 
 
 	def handle_join(self, args, event):
