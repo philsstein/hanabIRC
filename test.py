ec9a32f91137b47569602287f471a6bd7d00b0a6
Geoff Lawler <glawler@tislabs.com>
2013-01-25 12:54:12 -0500

    added simple test.py file for testing.

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from hanabi_game import Game
+from hanabi import Game
 from text_markup import xterm_markup
 
 def printLines(lines):
@@ -9,6 +9,10 @@ g = Game('foobar', xterm_markup())
 printLines(g.add_player('Olive'))
 printLines(g.show_game_state())
 printLines(g.add_player('Maisie'))
+printLines(g.add_player('Jasper'))
+printLines(g.add_player('George'))
+printLines(g.add_player('Frank'))
+printLines(g.add_player('One Too Many'))
 printLines(g.show_game_state())
 printLines(g.remove_player('Olive'))
 printLines(g.show_game_state())
