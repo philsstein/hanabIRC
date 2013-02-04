4b242371ab310b4d8f489648d18a8a7f5caee2a0
Geoff Lawler <glawler@tislabs.com>
2013-02-03 23:31:09 -0500

    added color and control codes for mIRC in comment for reference.

--- text_markup.py
+++ text_markup.py
@@ -52,7 +52,38 @@ class irc_markup(text_markup_base):
     mIRC specific markup encodings.
     See ascii_markup for usage examples.
     '''
-
+    # here are complete? color and control codes
+    # for mIRC. This si just for reference. We
+    # only support the hanabi colors for now.
+    # enum ColorCode {
+    #     White           =   0,   /**< White */
+    #     Black           =   1,   /**< Black */
+    #     DarkBlue        =   2,   /**< Dark blue */
+    #     DarkGreen       =   3,   /**< Dark green */
+    #     Red         =   4,   /**< Red */
+    #     DarkRed         =   5,   /**< Dark red */
+    #     DarkViolet      =   6,   /**< Dark violet */
+    #     Orange          =   7,   /**< Orange */
+    #     Yellow          =   8,   /**< Yellow */
+    #     LightGreen      =   9,   /**< Light green */
+    #     Cyan            =  10,   /**< Cornflower blue */
+    #     LightCyan       =  11,   /**< Light blue */
+    #     Blue            =  12,   /**< Blue */
+    #     Violet          =  13,   /**< Violet */
+    #     DarkGray            =  14,   /**< Dark gray */
+    #     LightGray       =  15   /**< Light gray */
+    # };
+    # 
+    # enum ControlCode {
+    #     Bold            = 0x02,     /**< Bold */
+    #     Color           = 0x03,     /**< Color */
+    #     Italic          = 0x09,     /**< Italic */
+    #     StrikeThrough           = 0x13,     /**< Strike-Through */
+    #     Reset           = 0x0f,     /**< Reset */
+    #     Underline       = 0x15,     /**< Underline */
+    #     Underline2      = 0x1f,     /**< Underline */
+    #     Reverse         = 0x16      /**< Reverse */
+    # }; 
     # from mIRC color codes
     _colormap = {
         text_markup_base.WHITE: 0,
