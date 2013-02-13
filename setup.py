f2cdb27628576319d7b1de3220502abf20c7939a
Geoff Lawler <glawler@tislabs.com>
2013-02-13 13:52:12 -0500

    ignore python setup.py generated files. add __init__.py for tests dir.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='HanabIRC',
     version='0.1',
-    packages=['hanabIRC',],
+    packages=find_packages(exclude=['test']), 
     license='FreeBSD License', 
     author='Geoff Lawler', 
     author_email='geoff.lawler@gmail.com',
     description='An IRC bot that organizes and plays the card game Hanabi.',
-    long_description=open('README').read(),
+    long_description=open('README.txt').read(),
     url='https://github.com/philsstein/hanabIRC',
-    packages=find_packages(exclude=['test*']), 
 )
