73d759126619841f464bc44aecbc5ed65f9dc0f8
Geoff Lawler <glawler@tislabs.com>
2013-02-13 13:20:49 -0500

    untested setup for distribution. just committing so I can oush to git hub.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='HanabIRC',
+    version='0.1',
+    packages=['hanabIRC',],
+    license='FreeBSD License', 
+    author='Geoff Lawler', 
+    author_email='geoff.lawler@gmail.com',
+    description='An IRC bot that organizes and plays the card game Hanabi.',
+    long_description=open('README').read(),
+    url= git repo URL
+    packages=find_packages(exclude=['test*']), 
+)
