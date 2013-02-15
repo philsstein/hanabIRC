bb9845d7d140f3a554630f609e43c8a8dd4034ff
Geoff Lawler <glawler@tislabs.com>
2013-02-15 17:29:40 -0500

    bumped version for PIP upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='HanabIRC',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(exclude=['*test']),
     license='FreeBSD License', 
     author='Geoff Lawler', 
